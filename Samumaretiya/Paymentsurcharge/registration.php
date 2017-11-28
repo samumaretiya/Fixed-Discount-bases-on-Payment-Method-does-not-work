b995dabdde9a195e843bc0f937b1e2dc8fda9607
sam umaretiya <samumaretiya@gmail.com>
2017-11-28 15:51:21 +0530

    removedextrastuff

--- Samumaretiya/Paymentsurcharge/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dcs_Paymentsurcharge',
-    __DIR__
-);
--- Samumaretiya/Paymentsurcharge/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Magento_Checkout/js/action/select-payment-method':
-                'Dcs_Paymentsurcharge/js/action/select-payment-method'
-        }
-    }
-};
